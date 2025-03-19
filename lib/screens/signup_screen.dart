af352f5ca4e218cf42600229f8d9725420abf97a
Kumud Raj Ghimire <kumudraj.cs23@bmsce.ac.in>
2025-03-19 18:10:05 +0530

    Basic login and signup

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,42 @@
+// lib/screens/signup_screen.dart
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Signup')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(decoration: InputDecoration(labelText: 'Username')),
+            TextField(decoration: InputDecoration(labelText: 'Email')),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle signup logic here
+              },
+              child: Text('Signup'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Navigate back to the login screen
+              },
+              child: Text('Already have an account? Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
